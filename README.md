c7b2ed6a290d908e75ded9f14b807d1abe9df39b
Le Rover <lerover1155@gmail.com>
2025-06-14 00:04:48 +0630

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # Hospital-Management-System-Frontend
+## Getting Started (with Bun)
+
+Better to use bun instead of npm and yarn 
+
+If you don't have bun, here's to install bun
+
+```bash
+curl -fsSL https://bun.sh/install | bash
+```
+After install, restart the terminal so bun is available.
+
+## Project Setup
+
+```bash
+git clone git@github.com:lerover/hospital-management-system.git
+
+```
+
+```bash
+cd hospital-management-system
+```
+
+```bash
+bun install
+```
+
+```bash
+bun run dev
+```
+
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { Button } from '@/components/ui/button'
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+
+const defaultValue = 'item-1'
+
+const accordionItems = [
+  { value: 'item-1', title: 'Is it accessible?', content: 'Yes. It adheres to the WAI-ARIA design pattern.' },
+  { value: 'item-2', title: 'Is it unstyled?', content: 'Yes. It\'s unstyled by default, giving you freedom over the look and feel.' },
+  { value: 'item-3', title: 'Can it be animated?', content: 'Yes! You can use the transition prop to configure the animation.' },
+]
+</script>
+
+<template>
+  <Button>Button</Button>
+  <Accordion type="single" class="w-full" collapsible :default-value="defaultValue">
+    <AccordionItem v-for="item in accordionItems" :key="item.value" :value="item.value">
+      <AccordionTrigger>{{ item.title }}</AccordionTrigger>
+      <AccordionContent>
+        {{ item.content }}
+      </AccordionContent>
+    </AccordionItem>
+  </Accordion>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
